8e1cf2d12f67b1c5f630781ea03a81b5499892ea
mebaoudjammar-svg <mebaoudjammar@gmail.com>
2025-11-26 20:59:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import QuizApp from "./src/QuizApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<QuizApp />);
